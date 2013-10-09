1eeac53b0329d92d8238b70ebfd3b251f1799585
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2013-10-09 16:10:18 +0400

    Added comment into config file

--- src/config/minify.php
+++ src/config/minify.php
@@ -2,32 +2,66 @@
 
 return array(
 
+	/*
+	|--------------------------------------------------------------------------
+	| The directory name relative to the root project directory
+	|--------------------------------------------------------------------------
+	|
+	| A good pratice is to put all files into public/assets
+	|
+	*/
     'assetsDirectory' => 'public/assets',
 
+	/*
+	|--------------------------------------------------------------------------
+	| Configurations for minify CSS
+	|--------------------------------------------------------------------------
+	*/
     'css' => array(
 
+    	/*
+    	|--------------------------------------------------------------------------
+    	| Directory list of all folder containing css file to minify, relative to assetsDirectory
+    	|--------------------------------------------------------------------------
+    	*/
         'directories' => array(
             'css'
         ),
 
+    	/*
+    	|--------------------------------------------------------------------------
+    	| You can pack multiple css file in once. It's recommandated to pack minified files
+    	|--------------------------------------------------------------------------
+    	*/
         'packages' => array(
-
-            // Default layout
             'css/common.pack.css' => array(
                 'css/common.min.css'
             )
         ),
     ),
 
+	/*
+	|--------------------------------------------------------------------------
+	| Configurations for minify JS
+	|--------------------------------------------------------------------------
+	*/
     'js' => array(
 
+    	/*
+    	|--------------------------------------------------------------------------
+    	| Directory list of all folder containing js file to minify, relative to assetsDirectory
+    	|--------------------------------------------------------------------------
+    	*/
         'directories' => array(
             'js'
         ),
 
+    	/*
+    	|--------------------------------------------------------------------------
+    	| You can pack multiple js file in once. It's recommandated to pack minified files
+    	|--------------------------------------------------------------------------
+    	*/
         'packages' => array(
-
-            // Default layout
             'js/common.pack.js' => array(
                 'js/console.min.js',
             )
